1900b7eb72e75d6ae637b4bd4a13439b577ea593
Elżbieta Kukułka <ellakaras@gmail.com>
2018-05-10 22:18:49 +0200

    Ready task 17_6 with styles

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+app.get('/auth/google', function(req, res){
+    res.render('google');
+});
+
+app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
